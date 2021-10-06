8add1782e0f1a9d2edd61baae6335760b268608b
simplysabir <76485932+simplysabir@users.noreply.github.com>
2021-10-06 02:36:29 -0700

    Create pattern-ques-01.java

---
+++ pattern-ques-01.java
@@ -0,0 +1,18 @@
+//pattern printing question no 1
+//we are using nested loop here
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+      int i = 0;
+      int j = 0;
+      for(i=0;i<4;i++){
+         
+          for(j=0;j<5;j++){
+              System.out.print("*");
+          }
+          System.out.print("\n");
+      }
+      
+    }
+}
