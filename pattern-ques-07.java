0b6c4f00d1ba7d7ccde17961bb2b291571248fa8
simplysabir <76485932+simplysabir@users.noreply.github.com>
2021-10-06 23:12:04 -0700

    Create pattern-ques-07.java

---
+++ pattern-ques-07.java
@@ -0,0 +1,22 @@
+//pattern question no 7
+
+public class MyClass {
+    public static void main(String args[]) {
+     int n = 5;
+     //int i = 1;
+     //int j = 1;
+     
+     for(int i=n;i>=1;i--){
+         
+         for(int j=1;j<=i;j++){
+             System.out.print(j+" ");
+         }
+         System.out.println();
+     }
+     
+     
+     
+     
+     
+    }
+}
